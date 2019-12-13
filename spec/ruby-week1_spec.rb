67720486bd79776006dbe79290491cbf973820dd
Colman Currie <curriecolman@gmail.com>
2019-12-13 12:24:46 -0800

    build and run test #2

--- spec/ruby-week1_spec.rb
+++ spec/ruby-week1_spec.rb
@@ -2,7 +2,14 @@ require ('rspec')
 require ('ruby-week1')
 
 describe('#ruby-week1') do
-  it('will check that one string is equal another') do
-    expect(solve.annagram_finder("a")).to(eq("a"))
+  it('will check that one letter is equal another') do
+    new_annagram = Annagram.new()
+    expect(new_annagram.annagram_finder("a")).to(eq("a"))
+  end
+end
+describe('#ruby-week1') do
+  it('will check that the letters of one word are equal another') do
+    new_annagram = Annagram.new()
+    expect(new_annagram.annagram_finder("care")).to(eq("race"))
   end
 end
