2069f6c29dc1fd666245595cd45c560a82bb2316
Colman Currie <curriecolman@gmail.com>
2019-12-13 14:45:21 -0800

    DRY up code

--- lib/ruby-week1.rb
+++ lib/ruby-week1.rb
@@ -5,8 +5,7 @@ class Anagram
       new_array = []
       words.each do |word2|
         if (word1.scan(/[aeiou]/i) !== new_array) && (word2.scan(/[aeiou]/i) !== new_array)
-          return
-          "Please input a real word, it must contain a vowel"
+          return ("Please input a real word, it must contain a vowel")
         end
       elsif (word2.downcase.split(//).sort == word1.downcase.split(//).sort)
           new_array.push(w2)
