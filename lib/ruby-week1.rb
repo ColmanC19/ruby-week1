cb8b4648072479d88d0fc054b46ce45f365bb99b
Colman Currie <curriecolman@gmail.com>
2019-12-13 12:25:19 -0800

    DRY up code

--- lib/ruby-week1.rb
+++ lib/ruby-week1.rb
@@ -1,7 +1,24 @@
 class Anagram
-def annagram_finder(word_array)
-  
-
-end
-
+  def annagram_finder(word_array)
+    if word_array.class != Array
+      word_array = word_array.split(" ")
+    end
+    result_array = []
+    word_array.each do |word|
+      x = 0
+      while(x < (word.length / 2).floor)
+        if (!(word[x] == word[(word.length - 1 - x)]))
+          result = false
+          break
+        else
+          result = true
+        end
+        x = x + 1
+      end
+      if result == true
+        result_array.push(word)
+      end
+    end
+    return result_array.join(" is an anagram of ")
+  end
 end
