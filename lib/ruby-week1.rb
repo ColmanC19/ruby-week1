739660ccd291122223679e251bab7977f97c9bce
Colman Currie <curriecolman@gmail.com>
2019-12-13 20:06:52 -0800

    fix syntax errors

--- lib/ruby-week1.rb
+++ lib/ruby-week1.rb
@@ -4,16 +4,16 @@ class Anagram
     words.each do |word1|
       new_array = []
       words.each do |word2|
-        if (word1.scan(/[aeiou]/i) !== new_array) && (word2.scan(/[aeiou]/i) !== new_array)
+        if (word1.scan(/[aeiou]/i) != new_array) && (word2.scan(/[aeiou]/i) != new_array)
           return ("Please input a real word, it must contain a vowel")
-        end
-      elsif (word2.downcase.split(//).sort == word1.downcase.split(//).sort)
+        elsif (word2.downcase.split(//).sort == word1.downcase.split(//).sort)
         new_array.push(w2)
-      end
-    elsif (word2.downcase.split(//).sort !== word1.downcase.split(//).sort)
+        elsif (word2.downcase.split(//).sort != word1.downcase.split(//).sort)
       return ("error, this is an Antigram")
     end
     result_array.push(new_array)
   end
   return result_array.uniq
+end
+end
 end
