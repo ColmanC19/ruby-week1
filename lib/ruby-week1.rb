9b7ba779588162ba25ff2ff24a88c06f3bda3077
Colman Currie <curriecolman@gmail.com>
2020-01-05 19:09:14 -0800

    fix indentation

--- lib/ruby-week1.rb
+++ lib/ruby-week1.rb
@@ -5,24 +5,24 @@ class Word
     @word2 = word2
   end
 
-	def anagram_finder
-		word_one = @word1.downcase.gsub(/[^a-z0-9]/i, '')
-		word_two = @word2.downcase.gsub(/[^a-z0-9]/i, '')
+  def anagram_finder
+    word_one = @word1.downcase.gsub(/[^a-z0-9]/i, '')
+    word_two = @word2.downcase.gsub(/[^a-z0-9]/i, '')
     new_word_one = word_one.chars.sort
     new_word_two = word_two.chars.sort
     new_check = new_word_one & new_word_two
-		if ((word_one.length) != (word_two.length))
-			return "These cannot be an anagram as their lengths do not match"
-		elsif (!contains_vowels(word_one) || !contains_vowels(word_two))
-			return "This is not a real word, please use a word containing a vowel"
-		elsif new_word_one.eql?(new_word_two)
-			return "This is an anagram"
-		elsif new_check.length == 0
-			return "This is an antigram"
+    if ((word_one.length) != (word_two.length))
+      return "These cannot be an anagram as their lengths do not match"
+    elsif (!contains_vowels(word_one) || !contains_vowels(word_two))
+      return "This is not a real word, please use a word containing a vowel"
+    elsif new_word_one.eql?(new_word_two)
+      return "This is an anagram"
+    elsif new_check.length == 0
+      return "This is an antigram"
     else
       return "These are neither an anagram nor an antigram, just two words with the same amount of letters in them."
-		end
-	end
+    end
+  end
 
   def contains_vowels(word)
     vowels = word.scan(/[aeiou]/)
