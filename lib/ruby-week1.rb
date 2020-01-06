2661b74c37e443f153e925a543fa73bb21f98e2d
Colman Currie <curriecolman@gmail.com>
2020-01-05 18:55:37 -0800

    refractoring of code complete

--- lib/ruby-week1.rb
+++ lib/ruby-week1.rb
@@ -8,17 +8,22 @@ class Word
 	def anagram_finder
 		word_one = @word1.downcase.gsub(/[^a-z0-9]/i, '')
 		word_two = @word2.downcase.gsub(/[^a-z0-9]/i, '')
+    new_word_one = word_one.chars.sort
+    new_word_two = word_two.chars.sort
+    new_check = new_word_one & new_word_two
 		if ((word_one.length) != (word_two.length))
 			return "These cannot be an anagram as their lengths do not match"
 		elsif (!contains_vowels(word_one) || !contains_vowels(word_two))
 			return "This is not a real word, please use a word containing a vowel"
-		elsif (word_one.chars.sort.join == (word_two.chars.sort.join))
+		elsif new_word_one.eql?(new_word_two)
 			return "This is an anagram"
-		else (word_one.chars.sort.join != (word_two.chars.sort.join))
+		elsif new_check.length == 0
 			return "This is an antigram"
+    else
+      return "These are neither an anagram nor an antigram, just two words with the same amount of letters in them."
 		end
 	end
-  
+
   def contains_vowels(word)
     vowels = word.scan(/[aeiou]/)
     if vowels.count() >= 1
